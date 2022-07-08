cc6bbfb0b89e3239afb9de6ee83e7c41cad8ae02
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2022-07-08 16:39:15 +0200

    basic operations done

--- assets/calculator.js
+++ assets/calculator.js
@@ -1,12 +1,17 @@
 buttons = document.querySelectorAll("button");
+const output = document.getElementsByClassName("output");
+
 let screenValue = "";
 let result = 0;
+
 for (item of buttons) {
     item.addEventListener("click", (e) => {
         btnPressed = e.target.innerText;
         if (btnPressed) {
             if (btnPressed === "=") {
+                console.log("output");
                 console.log("uguale", eval(result));
+                output[0].innerText = eval(result);
                 result = 0;
             } else if (btnPressed <= 9) {
                 result += btnPressed;
