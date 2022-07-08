c42f9757b2bcd935c161c53c3e051779d84ec353
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2022-07-08 15:02:10 +0200

    changing addEventListener

--- assets/calculator.js
+++ assets/calculator.js
@@ -1,30 +1,8 @@
-const numberEntered = document.getElementById("number_btn");
-const multiplyBtn = document.getElementById("multiplyBtn");
-const divideBtn = document.getElementById("divideBtn");
-const subtractBtn = document.getElementById("subtractBtn");
-const addBtn = document.getElementById("addBtn");
-// const percent = document.getElementById("percent");
-// const equal = document.getElementById("equal");
-// const open_bracket = document.getElementById("open_bracket");
-// const closing_bracket = document.getElementById("closing_bracket");
-// const dot = document.getElementById("dot");
-// const cancel = document.getElementById("cancel");
-// const output = document.getElementById("output");
 
-function add(e) {
-    console.log(3 + 4 + 6);
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+for (item of buttons) {
+    item.addEventListener("click", (e) => {
+        console.log(e.target.innerText, "has been pressed");
+    });
 }
-function substract(e) {
-    console.log("e", e);
-}
-function multiply(e) {
-    console.log("e", e);
-}
-function divide(e) {
-    console.log("e", e);
-}
-
-addBtn.addEventListener("click", add);
-subtractBtn.addEventListener("click", substract);
-multiplyBtn.addEventListener("click", multiply);
-divideBtn.addEventListener("click", divide);
