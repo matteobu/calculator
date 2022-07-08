0ef4e82509b824e28971a82d10e5441f8cd43ec2
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2022-07-08 15:49:50 +0200

    evaluation

--- assets/calculator.js
+++ assets/calculator.js
@@ -1,8 +1,20 @@
-
 buttons = document.querySelectorAll("button");
 let screenValue = "";
+let result = 0;
 for (item of buttons) {
     item.addEventListener("click", (e) => {
-        console.log(e.target.innerText, "has been pressed");
+        btnPressed = e.target.innerText;
+        if (btnPressed) {
+            if (btnPressed === "=") {
+                console.log("uguale", eval(result));
+                result = 0;
+            } else if (btnPressed <= 9) {
+                result += btnPressed;
+                console.log("button pressed: ", btnPressed);
+                console.log("numbers", result);
+            } else if (btnPressed == "." || "*" || "+" || "-" || "/") {
+                result += btnPressed;
+            }
+        }
     });
 }
